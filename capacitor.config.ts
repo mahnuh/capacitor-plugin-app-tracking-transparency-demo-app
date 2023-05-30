d04c28904e1fd35a7982c3ba2251e7dd40de508a
Manuel Heidrich <manuel@heidrich.dev>
2023-05-30 14:31:58 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-plugin-app-tracking-transparency-demo-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;

7727863b540f8ce1bcc1a08b9dfe5b846f7eeaf9
Manuel Heidrich <manuel@heidrich.dev>
2023-05-30 14:42:52 +0200

    Init Capacitor 5 demo app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {
+  AppTrackingStatusResponse,
+  AppTrackingTransparency,
+} from 'capacitor-plugin-app-tracking-transparency';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  public async getStatus(): Promise<AppTrackingStatusResponse> {
+    const response = await AppTrackingTransparency.getStatus();
+
+    console.log(response);
+    // { status: 'authorized' } for example
+
+    return response;
+  }
+
+  public async requestPermission(): Promise<AppTrackingStatusResponse> {
+    const response = await AppTrackingTransparency.requestPermission();
+
+    console.log(response);
+    // { status: 'authorized' } for example
+
+    return response;
+  }
 }
